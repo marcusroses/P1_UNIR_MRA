d1704e1b4897fda92f1579a56f808cefc817afb3
marcusroses <152766810+marcusroses@users.noreply.github.com>
2024-01-13 19:49:35 +0100

    Force error - blank space

--- app/api.py
+++ app/api.py
@@ -21,7 +21,7 @@ def add(op_1, op_2):
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
         return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
+        return           (str(e), http.client.BAD_REQUEST, HEADERS)
 
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
